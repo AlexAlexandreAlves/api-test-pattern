1c032032ff8de618421adb56ee78321893895138
fo057145 <3rdparty_alex.alves@inter.co>
2025-06-02 14:54:16 -0300

    remove prefix 'Bearer ' do token e ajusta importação nos testes

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,2 +1,2 @@
 export const BASE_URL = 'https://fakerestapi.azurewebsites.net/'
-export const TOKEN = 'Bearer YOUR_TOKEN_HERE'
+export const TOKEN = 'YOUR_TOKEN_HERE'
--- integration/tests/exemplo-uso-padrao.test.ts
+++ integration/tests/exemplo-uso-padrao.test.ts
@@ -1,11 +1,13 @@
 import { expect } from '@jest/globals';
 import * as request from 'supertest';
 import { BASE_URL } from '../../constants/constants';
+import { TOKEN } from '../../constants/constants';
 
 describe('Exemplos de teste usando o Get list', () => {
 
     it('Deve encontrar o registro e retornar o código de status 200', async () => {
-        const response = await request(BASE_URL).get('api/v1/Authors');
+        const response = await request(BASE_URL).get('api/v1/Authors')
+        // .set('Authorization', `Bearer ${TOKEN}`); Configura o TOKEN aqui
 
         expect(response.status).toBe(200);
         expect(response.body[0]).toEqual(expect.objectContaining(
@@ -22,6 +24,7 @@ describe('Exemplos de teste usando o Get list', () => {
 
         it('Deve encontrar o registro pelo id e retornar o código de status 200', async () => {
             const response = await request(BASE_URL).get('api/v1/Authors/2');
+            // .set('Authorization', `Bearer ${TOKEN}`); 
 
             expect(response.status).toBe(200);
             expect(response.body).toEqual(
@@ -36,6 +39,7 @@ describe('Exemplos de teste usando o Get list', () => {
 
         it('Deve tentar encontrar o registro pelo id, mas não encontrado e retornar o código de status 404', async () => {
             const response = await request(BASE_URL).get('api/v1/Authors/1000');
+            // .set('Authorization', `Bearer ${TOKEN}`); 
 
             expect(response.status).toBe(404);
             expect(response.body).toEqual(expect.objectContaining(
@@ -60,6 +64,7 @@ describe('Exemplos de teste usando o Get list', () => {
             };
 
             const response = await request(BASE_URL)
+                // .set('Authorization', `Bearer ${TOKEN}`); 
                 .post('/api/v1/Authors')
                 .send(novoAutor);
 
@@ -73,7 +78,6 @@ describe('Exemplos de teste usando o Get list', () => {
 
                 }
             ));
-
         });
 
         it('Deve tentar criar um autor com dados inválidos e verificar a resposta e o código de status 400', async () => {
@@ -85,6 +89,7 @@ describe('Exemplos de teste usando o Get list', () => {
             };
 
             const response = await request(BASE_URL)
+                // .set('Authorization', `Bearer ${TOKEN}`); 
                 .post('/api/v1/Authors')
                 .send(invalidAuthor);
 
