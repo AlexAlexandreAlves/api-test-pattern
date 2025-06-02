708294d00e3c2bf46e180b1bfbb656ca4b5000f3
fo057145 <3rdparty_alex.alves@inter.co>
2025-06-02 11:45:20 -0300

    adiciona testes de integração para operações com autores e refatora leitura de CSV

--- test/integration/exemplos-usando-data-driven/post-request-using-csv.test.ts
+++ test/integration/exemplos-usando-data-driven/post-request-using-csv.test.ts
@@ -3,6 +3,8 @@ import * as request from 'supertest';
 import { BASE_URL } from '../../../constants/constants';
 import * as path from 'path';
 import generics from '../../utils/generics';
+import { authorsRoute } from '../../routes/author-route';
+
 
 const randomId = Math.floor(Math.random() * 1000) + 1;
 
@@ -10,31 +12,30 @@ describe('Post request example test with CSV', () => {
 
     it('Should create authors from CSV file and check response data and status code 200', async () => {
 
-        const filePath = path.join(__dirname, '../data/csv/authors.csv');
+        const filePath = path.join(__dirname, '../../data/csv/authors.csv');
         const authors = await generics.readCsvFile(filePath);
 
         for (const author of authors) {
             const novoAutor = {
                 id: randomId,
-                idBook: randomId,
+                idBook: 1,
                 firstName: author.firstName,
                 lastName: author.lastName
             };
 
             const response = await request(BASE_URL)
-                .post('/api/v1/Authors')
+                .post(authorsRoute.createAuthors)
                 .send(novoAutor);
                 
                 console.log(response.body);
 
-
             expect(response.status).toBe(200);
-            // expect(response.body).toEqual(expect.objectContaining({
-            //     id: randomId,
-            //     idBook: randomId,
-            //     firstName: author.firstName,
-            //     lastName: author.lastName
-            // }));
+            expect(response.body).toEqual(expect.objectContaining({
+                id: randomId,
+                idBook: 1,
+                firstName: author.firstName,
+                lastName: author.lastName
+            }));
         }
     });
 });
