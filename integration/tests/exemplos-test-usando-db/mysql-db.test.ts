7552984daab5dffbe807990fb92b54709bc0ae50
fo057145 <3rdparty_alex.alves@inter.co>
2025-06-02 15:17:07 -0300

    removendo arquivos de config de banco, este fluxo foi separado e esta em uma branch isolada

--- integration/tests/exemplos-test-usando-db/mysql-db.test.ts
+++ integration/tests/exemplos-test-usando-db/mysql-db.test.ts
@@ -6,11 +6,9 @@ describe('Teste de conexão MYSQL DB', () => {
         await mysqlPool.end(); // fecha a conexão ao fim dos testes
     });
 
-    it('Deve buscar o usuário com id = 1', async () => {
-        const [rows] = await mysqlPool.query('SELECT name FROM users WHERE id = ?', [1]);
-        const user = (rows as any[])[0];
-
-        expect(user.name).toBe('Ana Paula'); // Busca na base de dados teste criada via docker
+    it('Deve conectar ao banco e retornar 1', async () => {
+        const [rows] = await mysqlPool.query('SELECT 1 as result');
+        expect((rows as any[])[0].result).toBe(1);
     });
 
 });
--- integration/tests/exemplos-test-usando-db/postgres-db.test.ts
+++ integration/tests/exemplos-test-usando-db/postgres-db.test.ts
@@ -9,12 +9,4 @@ describe('Teste de conexão postgres DB', () => {
   it('Deve conectar com sucesso', async () => {
     await expect(testConnection()).resolves.not.toThrow();
   });
-
-  it('Deve retornar o usuário com id = 1 e nome = "Ana Souza"', async () => {
-    const result = await pgPool.query('SELECT name FROM users WHERE id = $1', [1]);
-
-    expect(result.rows.length).toBe(1);
-    expect(result.rows[0].name).toBe('Ana Souza');
-  });
-
 });
